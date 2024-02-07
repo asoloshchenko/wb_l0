633c4372a7aecb394f6165deacacee7a7b86f120
soloshenko.ad <soloshenko.ad@gmail.com>
2024-02-07 18:13:26 +0300

    v0

--- cmd/test0/main.go
+++ cmd/test0/main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log/slog"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
-	"test/internal/cache"
-	"test/internal/model"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 
+	"test/internal/cache"
 	"test/internal/config"
+	"test/internal/model"
 	"test/internal/postgres"
 
 	"github.com/nats-io/stan.go"
@@ -33,8 +37,8 @@ func main() {
 	}
 
 	//TODO: init cache
-	c := cache.New(time.Minute*10, time.Minute*5) //TODO: read time from config
-	err = c.Restore(storage)
+	inCache := cache.New(time.Minute*10, time.Minute*5) //TODO: read time from config
+	err = inCache.Restore(storage)
 	if err != nil {
 		slog.Error(err.Error())
 		os.Exit(1)
@@ -44,13 +48,13 @@ func main() {
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
+	slog.Info("Subscribing...")
 	nc, err := stan.Connect("test-cluster", "1") //TODO: read name from config
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 
-	fmt.Println("start")
 	nc.Subscribe("foo", func(m *stan.Msg) {
 		var msg model.DataStruct
 
@@ -62,15 +66,71 @@ func main() {
 			slog.Info("OrderUID is empty")
 			return
 		}
-
-		c.Set(msg.OrderUID, msg, 0)
+		inCache.Set(msg.OrderUID, msg, 0)
 		storage.WriteMessage(msg)
-		//fmt.Println(len(msg.Items))
 
-		//fmt.Printf("Received a message: %s\n", string(m.Data))
 	})
 
+	slog.Info("Starting server...")
+
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := chi.URLParam(r, "id")
+
+		if id == "" {
+			w.Write([]byte("empty id"))
+			return
+		}
+		if value, ok := inCache.Get(id); ok {
+			response, err := json.Marshal(value)
+
+			if err != nil {
+				w.Write([]byte(err.Error()))
+				return
+			}
+
+			w.Write(response)
+			return
+		}
+
+		if value, err := storage.GetMessageByID(id); err == nil {
+			response, err := json.Marshal(value)
+
+			if err != nil {
+				w.Write([]byte(err.Error()))
+				return
+			}
+
+			w.Write(response)
+			return
+		}
+
+		w.Write([]byte("No such id"))
+
+		//w.Write([]byte("welcome"))
+	})
+
+	srv := &http.Server{
+		Addr:    ":3333",
+		Handler: r,
+	}
+
+	http.ListenAndServe(":3333", r)
+
+	slog.Info("Server is on port 3333")
+
 	<-done
-	fmt.Println("stop")
+	slog.Info("stopping server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		slog.Info("failed to stop server")
+	}
 
 }
